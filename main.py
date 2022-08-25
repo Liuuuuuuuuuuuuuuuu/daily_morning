73772108604278652d08c77c92ab71e55d4e53de
Heqiang <55489725+theheqiang@users.noreply.github.com>
2022-08-25 08:53:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,48 +6,100 @@ import requests
 import os
 import random
 
-today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
+
+city_id = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+appid = os.environ["APP_ID"]
+secret = os.environ["APP_SECRET"]
 
-user_id = os.environ["USER_ID"]
+touser = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
+info="单身狗也要对自己好一点~"#底部信息
+e='Single people should be kind to themselves'#底部信息
 
+grant_type = 'client_credential'
+url=f'https://api.weixin.qq.com/cgi-bin/token?grant_type={grant_type}&appid={appid}&secret={secret}'
+response=requests.get(url).json()
+access_token=''
+if response['expires_in']==7200:
+    # 获取token
+    access_token=response['access_token']
+    #天气获取
+    headers = {
+        'Referer': 'http://www.weather.com.cn/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
+    }
+    url = f'http://d1.weather.com.cn/weather_index/{city_id}.html?_=' + str(int(round(time.time() * 1000)))
+    #日期计算
+    r = requests.get(url, headers=headers)
+    r.encoding = "utf8"
+    res = eval(r.text.split(";")[0].split("=")[-1])
+    time=res['weatherinfo']['fctime']
+    year=time[0:4]
+    month=time[4:6]
+    day=time[6:8]
+    week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+    week=week_list[datetime.date(int(year),int(month),int(day)).weekday()]
+    date = f'{year}年{month}月{day}日  {week}'
+    date2 = f'{int(month)}月{day}日'
 
-def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    # 你活了多久
+    birthday_date = datetime.datetime.strptime(birthday, "%Y-%m-%d")
+    curr_datetime = datetime.datetime.now()
+    minus_datetime = curr_datetime - birthday_date
+    #发送消息
+    sendMessage_url=f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
+    for user in touser:
+        data={
+           "touser":user,
+           "template_id":template_id,
+           "appid":appid,
+           "data":{
+               "demo": {
+                   "value": "测试语句",
+                   "color": "#AAFF00"
+               },
+               "live": {
+                   "value": minus_datetime.days,
+                   "color": "#00FF00"
+               },
+               "date2": {
+                   "value": date2,
+                   "color": "#6B6A66"
+               },
+               "date": {
+                   "value": date,
+                   "color": "#CAA06A"
+               },
+                "city":{
+                    "value":res['weatherinfo']['city'],
+                    "color": "#00FF00"
+                },
+               "weather":{
+                   "value":res['weatherinfo']['weather'],
+                   "color": "#F9AD08"
+               },
+                "tempn":{
+                    "value":res['weatherinfo']['tempn'],
+                    "color": "#9DB981"
+                },
+               "temp":{
+                    "value":res['weatherinfo']['temp'],
+                    "color":"#CAA06A"
+                },
+               "wd":{
+                    "value":info,
+                    "color":"#92CAD9"
+                },
+               'english':{
+                   "value":e,
+                   "color":"#FF0000"
+               }
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+           }
+       }
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
-
-def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
-
-def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
-
-
-client = WeChatClient(app_id, app_secret)
-
-wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+        getTemp=requests.post(sendMessage_url,data=json.dumps(data)).json()
+else:
+    print("appid或secret错误")
