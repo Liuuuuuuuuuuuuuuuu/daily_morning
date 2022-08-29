b55a9ed472056b3faeff0249e18978184af8de57
Heqiang <55489725+theheqiang@users.noreply.github.com>
2022-08-29 11:14:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,164 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+import math
+from wechatpy import WeChatClient, WeChatClientException
+from wechatpy.client.api import WeChatMessage
 import requests
 import os
 import random
 
+nowtime = datetime.utcnow() + timedelta(hours=8)  # 东八区时间
+today = datetime.strptime(str(nowtime.date()), "%Y-%m-%d") #今天的日期
 
-city_id = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+start_date = os.getenv('START_DATE')
+city = os.getenv('CITY')
+birthday = os.getenv('BIRTHDAY')
 
-appid = os.environ["APP_ID"]
-secret = os.environ["APP_SECRET"]
+app_id = os.getenv('APP_ID')
+app_secret = os.getenv('APP_SECRET')
 
-touser = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
-info="单身狗也要对自己好一点~"#底部信息
-e='Single people should be kind to themselves'#底部信息
+user_ids = os.getenv('USER_ID', '').split("\n")
+template_id = os.getenv('TEMPLATE_ID')
 
-grant_type = 'client_credential'
-url=f'https://api.weixin.qq.com/cgi-bin/token?grant_type={grant_type}&appid={appid}&secret={secret}'
-response=requests.get(url).json()
-access_token=''
-if response['expires_in']==7200:
-    # 获取token
-    access_token=response['access_token']
-    #天气获取
-    headers = {
-        'Referer': 'http://www.weather.com.cn/',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
-    }
-    url = f'http://d1.weather.com.cn/weather_index/{city_id}.html?_=' + str(int(round(time.time() * 1000)))
-    #日期计算
-    r = requests.get(url, headers=headers)
-    r.encoding = "utf8"
-    res = eval(r.text.split(";")[0].split("=")[-1])
-    time=res['weatherinfo']['fctime']
-    year=time[0:4]
-    month=time[4:6]
-    day=time[6:8]
-    week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
-    week=week_list[datetime.date(int(year),int(month),int(day)).weekday()]
-    date = f'{year}年{month}月{day}日  {week}'
-    date2 = f'{int(month)}月{day}日'
+if app_id is None or app_secret is None:
+  print('请设置 APP_ID 和 APP_SECRET')
+  exit(422)
 
-    # 你活了多久
-    birthday_date = datetime.datetime.strptime(birthday, "%Y-%m-%d")
-    curr_datetime = datetime.datetime.now()
-    minus_datetime = curr_datetime - birthday_date
-    #发送消息
-    sendMessage_url=f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
-    for user in touser:
-        data={
-           "touser":user,
-           "template_id":template_id,
-           "appid":appid,
-           "data":{
-               "demo": {
-                   "value": "测试语句",
-                   "color": "#AAFF00"
-               },
-               "live": {
-                   "value": minus_datetime.days,
-                   "color": "#00FF00"
-               },
-               "date2": {
-                   "value": date2,
-                   "color": "#6B6A66"
-               },
-               "date": {
-                   "value": date,
-                   "color": "#CAA06A"
-               },
-                "city":{
-                    "value":res['weatherinfo']['city'],
-                    "color": "#00FF00"
-                },
-               "weather":{
-                   "value":res['weatherinfo']['weather'],
-                   "color": "#F9AD08"
-               },
-                "tempn":{
-                    "value":res['weatherinfo']['tempn'],
-                    "color": "#9DB981"
-                },
-               "temp":{
-                    "value":res['weatherinfo']['temp'],
-                    "color":"#CAA06A"
-                },
-               "wd":{
-                    "value":info,
-                    "color":"#92CAD9"
-                },
-               'english':{
-                   "value":e,
-                   "color":"#FF0000"
-               }
+if not user_ids:
+  print('请设置 USER_ID，若存在多个 ID 用回车分开')
+  exit(422)
 
-           }
-       }
+if template_id is None:
+  print('请设置 TEMPLATE_ID')
+  exit(422)
 
-        getTemp=requests.post(sendMessage_url,data=json.dumps(data)).json()
-else:
-    print("appid或secret错误")
+# weather 直接返回对象，在使用的地方用字段进行调用。
+def get_weather():
+  if city is None:
+    print('请设置城市')
+    return None
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  if res is None:
+    return None
+  weather = res['data']['list'][0]
+  return weather
+
+# 获取当前日期为星期几
+def get_week_day():
+  week_list = ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"]
+  week_day = week_list[datetime.date(today).weekday()]
+  return week_day
+
+# 纪念日正数
+def get_memorial_days_count():
+  if start_date is None:
+    print('没有设置 START_DATE')
+    return 0
+  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+  return delta.days
+
+# 生日倒计时
+def get_birthday_left():
+  if birthday is None:
+    print('没有设置 BIRTHDAY')
+    return 0
+  next = datetime.strptime(str(today.year) + "-" + birthday, "%Y-%m-%d")
+  if next < nowtime:
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+# 彩虹屁 接口不稳定，所以失败的话会重新调用，直到成功
+def get_words():
+  words = requests.get("https://api.shadiao.pro/chp")
+  if words.status_code != 200:
+    return get_words()
+  return words.json()['data']['text']
+
+def format_temperature(temperature):
+  return math.floor(temperature)
+
+# 随机颜色
+def get_random_color():
+  return "#%06x" % random.randint(0, 0xFFFFFF)
+
+try:
+  client = WeChatClient(app_id, app_secret)
+except WeChatClientException as e:
+  print('微信获取 token 失败，请检查 APP_ID 和 APP_SECRET，或当日调用量是否已达到微信限制。')
+  exit(502)
+
+wm = WeChatMessage(client)
+weather = get_weather()
+if weather is None:
+  print('获取天气失败')
+  exit(422)
+data = {
+  "city": {
+    "value": city,
+    "color": get_random_color()
+  },
+  "date": {
+    "value": today.strftime('%Y年%m月%d日'),
+    "color": get_random_color()
+  },
+  "week_day": {
+    "value": get_week_day(),
+    "color": get_random_color()
+  },
+  "weather": {
+    "value": weather['weather'],
+    "color": get_random_color()
+  },
+  "humidity": {
+    "value": weather['humidity'],
+    "color": get_random_color()
+  },
+  "wind": {
+    "value": weather['wind'],
+    "color": get_random_color()
+  },
+  "air_data": {
+    "value": weather['airData'],
+    "color": get_random_color()
+  },
+  "air_quality": {
+    "value": weather['airQuality'],
+    "color": get_random_color()
+  },
+  "temperature": {
+    "value": math.floor(weather['temp']),
+    "color": get_random_color()
+  },
+  "highest": {
+    "value": math.floor(weather['high']),
+    "color": get_random_color()
+  },
+  "lowest": {
+    "value": math.floor(weather['low']),
+    "color": get_random_color()
+  },
+  "love_days": {
+    "value": get_memorial_days_count(),
+    "color": get_random_color()
+  },
+  "birthday_left": {
+    "value": get_birthday_left(),
+    "color": get_random_color()
+  },
+  "words": {
+    "value": get_words(),
+    "color": get_random_color()
+  },
+}
+
+if __name__ == '__main__':
+  count = 0
+  try:
+    for user_id in user_ids:
+      res = wm.send_template(user_id, template_id, data)
+      count+=1
+  except WeChatClientException as e:
+    print('微信端返回错误：%s。错误代码：%d' % (e.errmsg, e.errcode))
+    exit(502)
+
+  print("发送了" + str(count) + "条消息")
